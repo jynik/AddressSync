ee007cd196a5e60f04c592dc474b7453cef7770f
Jon Szymaniak <jon.szymaniak.foss@gmail.com>
2021-05-18 23:41:21 -0500

    src: Added source files

---
+++ src/main/java/addresssync/AddressSyncPlugin.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package addresssync;
+
+
+import ghidra.app.plugin.PluginCategoryNames;
+import ghidra.app.plugin.ProgramPlugin;
+import ghidra.framework.plugintool.*;
+import ghidra.framework.plugintool.util.PluginStatus;
+import ghidra.program.util.ProgramLocation;
+
+
+//@formatter:off
+@PluginInfo(
+	status = PluginStatus.UNSTABLE,
+	packageName = "AddressSync",
+	category = PluginCategoryNames.SELECTION,
+	shortDescription = "Synchronize currently selected address to external program",
+	description = "Listens on localhost:1080 (UDP) for 64-bit address and sets cursor to specified address."
+)
+//@formatter:on
+public class AddressSyncPlugin extends ProgramPlugin {
+
+	SyncServer server;
+
+	/**
+	 * Plugin constructor.
+	 *
+	 * @param tool The plugin tool that this plugin is added to.
+	 */
+	public AddressSyncPlugin(PluginTool tool) {
+		super(tool, true, true);
+		server = new SyncServer(tool, 1080);
+	}
+
+	@Override
+	public void init() {
+		super.init();
+		server.start();
+	}
+
+	@Override
+	protected void locationChanged(ProgramLocation loc)
+	{
+		if (loc != null) {
+			server.setCurrentProgram(loc.getProgram());
+		} else {
+			server.setCurrentProgram(null);
+		}
+	}
+}
---
+++ src/main/java/addresssync/SyncServer.java
@@ -0,0 +1,80 @@
+package addresssync;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
+import ghidra.app.services.GoToService;
+import ghidra.framework.plugintool.PluginTool;
+import ghidra.program.model.address.Address;
+import ghidra.program.model.address.AddressOutOfBoundsException;
+import ghidra.program.model.listing.Program;
+
+public class SyncServer extends Thread {
+	private int port;
+	private PluginTool plugin;
+	private Program currProgram;
+
+	public SyncServer(PluginTool plugin, int port) {
+		this.port = port;
+		this.plugin = plugin;
+		this.currProgram = null;
+	}
+
+	public void setCurrentProgram(Program p) {
+		this.currProgram = p;
+	}
+
+	@SuppressWarnings("resource")
+	public void run() {
+		// We'll always operate on 64-bit little endian addresses.
+		byte[] addrBuf = new byte[8];
+		long address;
+
+		DatagramSocket sock;
+
+		try {
+			sock = new DatagramSocket(null);
+			InetSocketAddress sockAddr = new InetSocketAddress("localhost", 1080);
+			sock.bind(sockAddr);
+		} catch (SocketException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		while (true) {
+			DatagramPacket pkt = new DatagramPacket(addrBuf, addrBuf.length);
+			try {
+				sock.receive(pkt);
+				address =   (addrBuf[0] & 0xff)	        |
+					       ((addrBuf[1] & 0xff) << 8)   |
+					       ((addrBuf[2] & 0xff) << 16)  |
+					       ((addrBuf[3] & 0xff) << 24)  |
+					       ((addrBuf[4] & 0xff) << 32)  |
+					       ((addrBuf[5] & 0xff) << 40)  |
+					       ((addrBuf[6] & 0xff) << 48)  |
+					       ((addrBuf[7] & 0xff) << 56);
+
+				// FIXME: Race condition on this access
+				if (this.currProgram == null) {
+					continue;
+				}
+
+				try {
+					Address loc = this.currProgram.getImageBase().add(address);
+					this.plugin.getService(GoToService.class).goTo(loc);
+				} catch (AddressOutOfBoundsException e) {
+					System.err.printf("[AddressSync] Could not set address: 0x%08x\n", address);
+					// System.err.println(e.getMessage());
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
